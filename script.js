e7ea6805d0dfdce378647fc6ee98e85992bf8a6b
SSUHOY <sammysoul96@gmail.com>
2023-05-22 20:34:41 +0300

    Добавляет рендер html из js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Рендер страницы 
+
+const renderApp = () => {
+    const appEl = document.getElementById('gameApp') 
+
+    const appHtml = `
+    <section class="difficult__items-block center">
+    <div class="container">
+        <form class="difficult__values-container">
+            <legend class="dif-header">Выбери сложность</legend>
+            <fieldset class="difficult__values-form">
+                <div>
+                    <input type="radio" value="1" id="1" class="value_1"/>
+                    <label for="1" class="difficult__values-buttons">1</label>
+                </div>
+                <div>
+                    <input type="radio" value="2" id="2" class="value_2"/>
+                    <label for="2" class="difficult__values-buttons">2</label>
+                </div>
+                <div>
+                    <input type="radio" value="3" id="3" class="value_3"/>
+                    <label for="3" class="difficult__values-buttons">3</label>
+                </div>
+            </fieldset>
+            <div class="button-container">
+                <button class="start-button">Старт</button>
+            </div>
+        </form>
+    </div>
+</section>
+    `
+   appEl.innerHTML = appHtml;
+}
+
+renderApp();
+
 const initDifficultBtn = () => {
     const diffButtons = document.querySelectorAll(".difficult__values-buttons")
     for (const diffButton of diffButtons) {
@@ -8,7 +44,6 @@ const initDifficultBtn = () => {
     }
 
 }
-
 initDifficultBtn();
 
 
@@ -20,9 +55,10 @@ initStartBtn = () => {
             event.stopPropagation();
 
             console.log('Start');
+            renderGameApp();
         });
 
     }
 }
-
 initStartBtn();
+
