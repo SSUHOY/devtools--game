874af9bbf090bb8e34f8497fd555927e330f4e3a
SSUHOY <sammysoul96@gmail.com>
2023-06-05 19:21:51 +0300

    Дорабатывает стили и плагины

---
+++ js/components/difficulty-page-component.js
@@ -0,0 +1,75 @@
+import { game } from '../main.js'
+import { GAME_PAGE } from '../routes.js'
+import { createDeck } from '../helpers.js'
+
+export function renderDifficultPageComponent({ appEl, goToPage }) {
+    const appHtml = `<div class="difficult__items-block center">
+    <form class="container">
+                    <div class="difficult__values-container">
+                        <p class="dif-header">Выбери сложность</p>
+                        <div class="difficult__values">
+                            <label class="difficult__values-buttons">
+                                <input type="radio" name="difficulty" value="1" class="value" />
+                                <span class="difficulty-box__item-text">1</span>
+                            </label>
+                            <label class="difficult__values-buttons">
+                                <input type="radio" name="difficulty" value="2" class="value" />
+                                <span class="difficulty-box__item-text">2</span>
+                            </label>
+                            <label class="difficult__values-buttons">
+                                <input type="radio" name="difficulty" value="3" class="value"/>
+                                <span class="difficulty-box__item-text">3</span>
+                            </label>
+                        </div>
+                        <div class = 'difficult__btn'>
+                        <button type="submit" class="start-button" id="start-btn">Старт</button>
+                        </div>
+                        <div class='form-error'></div> 
+                    </div>
+                </form>
+                </div>`
+
+    appEl.innerHTML = appHtml
+
+    const difficultyFormEl = appEl.querySelector('.container')
+    const formErrorEl = appEl.querySelector('.form-error')
+
+    const difficultyBtnElements = document.querySelectorAll(
+        '.difficult__values-buttons'
+    )
+
+    let prevSelectedBtn = null
+
+    difficultyBtnElements.forEach((difficultyBtnEl) => {
+        difficultyBtnEl.addEventListener('click', () => {
+            setError('')
+            if (prevSelectedBtn !== null) {
+                prevSelectedBtn.classList.remove('selected')
+            }
+            difficultyBtnEl.classList.add('selected')
+            prevSelectedBtn = difficultyBtnEl
+        })
+    })
+
+    difficultyFormEl.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const selectedDifficultyBtn = difficultyFormEl.querySelector(
+            "input[name='difficulty']:checked"
+        )
+
+        if (!selectedDifficultyBtn) {
+            setError('Выберите уровень сложности!')
+            return
+        }
+
+        const selectedDifficulty = parseInt(selectedDifficultyBtn.value)
+        game.difficulty = selectedDifficulty
+        game.gameStatus = GAME_PAGE
+        game.cards = createDeck(selectedDifficulty)
+        goToPage(GAME_PAGE)
+    })
+
+    function setError(message) {
+        formErrorEl.textContent = message
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,60 @@
-/* eslint-disable prettier/prettier */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-
-const isProduction = process.env.NODE_ENV === "production"
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    entry: "./main.js",
-    mode: isProduction ? "production" : "development",
+    entry: './js/main.js',
+    mode: isProduction ? 'production' : 'development',
     output: {
-        // eslint-disable-next-line no-undef
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
         clean: true,
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
             },
         ],
     },
+    devtool:
+        process.env.NODE_ENV === 'production'
+            ? 'hidden-source-map'
+            : 'source-map',
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    // eslint-disable-next-line no-dupe-keys
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    node: {
+        __dirname: true,
+    },
     plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
         new HtmlWebpackPlugin({
-            template: "./index.html",
+            template: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'mini.css',
         }),
     ],
-    devtool: isProduction ? "hidden-source-map": "source-map"
 }
