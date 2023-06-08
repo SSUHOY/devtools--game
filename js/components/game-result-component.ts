46468e2cc158f7999b4c2ec2974056a8271d1a2e
SSUHOY <sammysoul96@gmail.com>
2023-06-08 20:53:10 +0300

    Добавляет стили к форме "выиграл/проиграл"

---
+++ js/components/game-result-component.ts
@@ -0,0 +1,27 @@
+// import { DIFFICULTY_PAGE } from '../routes.ts'
+// import { game } from '../main.ts'
+
+// export function renderResultMessagePageComp({ appEl, goToPage }) {
+//     const appHtml = `<div class = "main">
+//     <div class = 'box'>
+//         <img src="dist/static/img/win-img.svg" alt="win-img">
+//         <p class="box__title result-box__title">${
+//             game.isWin ? 'Вы выиграли!' : 'Вы проиграли!'
+//         }</p>
+//         <p class="timer_title"></p>
+//             <div class="timer time-result">
+//                 <div class="timer__block">
+                         
+//                     <span id="minutes">00</span>
+//                   </div>
+//                   <span class="timer__dot">.</span>
+//                   <div class="timer__block">
+                   
+//                     <span id="seconds">00</span>
+//                   </div>
+//             </div>
+//             <button class="submit-button"></button>
+//     </div>
+// </div>
+//     `
+// }
