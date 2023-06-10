99f35453be5fc24dea2c2cea097be9248e638f18
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-06-10 13:07:58 +0300

    Добавляет файл с тестами

--- main-component.test.js
+++ main-component.test.js
@@ -1,14 +1,17 @@
-const { it, describe } = require('@jest/globals')
-const {
-    renderDifficultPageComponent,
-} = require('./difficulty-page-component')
+// Здесь будут тесты
 
-describe("renderComponent", () => {
-    describe('renderDifficultPageComponent()', () => {
-        it('should render difficult page'), () => {
-            const start = renderDifficultPageComponent();
+
+// const { it, describe } = require('@jest/globals')
+// const {
+//     renderDifficultPageComponent,
+// } = require('./difficulty-page-component')
+
+// describe("renderComponent", () => {
+//     describe('renderDifficultPageComponent()', () => {
+//         it('should render difficult page'), () => {
+//             const start = renderDifficultPageComponent();
 
         
-        } 
-    })
-})
+//         } 
+//     })
+// })
